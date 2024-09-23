654e4563d24e9d0038787b42cfe034bbbbd4e6ee
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-23 10:55:08 -0500

    Add FencerService.kt

---
+++ src/main/kotlin/com/sphirye/springtemplate/service/FencerService.kt
@@ -0,0 +1,22 @@
+package com.sphirye.springtemplate.service
+
+import com.sphirye.springtemplate.model.Fencer
+import com.sphirye.springtemplate.repository.FencerRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+
+@Service
+class FencerService(
+    private val _fencerRepository: FencerRepository
+) {
+
+    fun searchByName(name: String?, pageRequest: PageRequest): Page<Fencer> {
+        return if (name.isNullOrEmpty()) {
+            _fencerRepository.findAll(pageRequest)
+        } else {
+            _fencerRepository.findAllByNameIgnoreCaseContaining(name, pageRequest)
+        }
+    }
+
+}
