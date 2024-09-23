ef5414b9de740d324f2307ac9c8e5f765124e623
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-23 11:35:43 -0500

    Add missing @Repository to CombatRepository.kt

--- src/main/kotlin/com/sphirye/springtemplate/repository/CombatRepository.kt
+++ src/main/kotlin/com/sphirye/springtemplate/repository/CombatRepository.kt
@@ -2,7 +2,9 @@ package com.sphirye.springtemplate.repository
 
 import com.sphirye.springtemplate.model.Combat
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CombatRepository : JpaRepository<Combat, Long> {
 
 }
