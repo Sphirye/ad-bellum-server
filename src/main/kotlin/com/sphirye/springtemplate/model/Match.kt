7b0070f28cfa9c2fc3dceadf8e76212d9217c8db
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-24 12:03:15 -0500

    Refactor Match.kt constraint definition over fencer_1 and fencer_2 fields

--- src/main/kotlin/com/sphirye/springtemplate/model/Match.kt
+++ src/main/kotlin/com/sphirye/springtemplate/model/Match.kt
@@ -1,7 +1,10 @@
 package com.sphirye.springtemplate.model
 
 import com.sphirye.shared.utils.Identifiable
+import com.sphirye.shared.web.annotation.EntityExists
 import jakarta.persistence.*
+import net.minidev.json.annotate.JsonIgnore
+import java.io.Serializable
 
 @Entity
 class Match (
@@ -9,19 +12,36 @@ class Match (
     @GeneratedValue
     override var id: Long? = null,
 
-    @ManyToOne
-    @JoinColumn(name = "fencer_1_id")
-    var fencer_1: Fencer? = null,
+    @Column(name = "fencer_1", nullable = false)
+    @field:EntityExists(
+        entityName = "Fencer",
+        primaryKey = "id",
+    )
+    var fencer_1_id: Long? = null,
 
-    @ManyToOne
-    @JoinColumn(name = "fencer_2_id")
-    var fencer_2: Fencer? = null,
+    @Column(name = "fencer_2", nullable = false)
+    @field:EntityExists(
+        entityName = "Fencer",
+        primaryKey = "id",
+    )
+    var fencer_2_id: Long? = null,
 
     @Enumerated(EnumType.STRING)
     var state: MatchState? = null,
 
-): Identifiable<Long> {
+): Identifiable<Long>, Serializable {
     enum class MatchState {
         WAITING, IN_PROGRESS, FINISHED
     }
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "fencer_1", insertable = false, updatable = false)
+    var fencer_1: Fencer? = null
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "fencer_2", insertable = false, updatable = false)
+    var fencer_2: Fencer? = null
+
 }
