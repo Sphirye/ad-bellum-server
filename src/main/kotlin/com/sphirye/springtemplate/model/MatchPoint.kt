2c8b509bec29fb9c9212bd3dff5fc90724b801b2
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-24 00:29:43 -0500

    Add MatchPoint.kt

---
+++ src/main/kotlin/com/sphirye/springtemplate/model/MatchPoint.kt
@@ -0,0 +1,41 @@
+package com.sphirye.springtemplate.model
+
+import com.sphirye.shared.utils.Identifiable
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import org.jetbrains.annotations.NotNull
+
+@Entity
+class MatchPoint (
+    @Id
+    @GeneratedValue
+    override var id: Long? = null,
+
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    var type: PointType? = null,
+
+    @field:NotNull
+    @ManyToOne
+    var scorer: Fencer? = null,
+
+    @field:NotNull
+    var timestamp: String? = null,
+
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    var verdict: Verdict? = null,
+
+): Identifiable<Long> {
+    enum class PointType {
+        CUT, THRUST, SLICE
+    }
+
+    enum class Verdict {
+        CLEAN, AFTER_BLOW, DOUBLE, NO_EXCHANGE
+    }
+}
