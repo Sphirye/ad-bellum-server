fe7ac7b71158b9ef6ab03690d5a1e7925f1b3860
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-24 11:00:09 -0500

    Add POST fencer endpoint

--- src/main/kotlin/com/sphirye/springtemplate/controller/FencerController.kt
+++ src/main/kotlin/com/sphirye/springtemplate/controller/FencerController.kt
@@ -7,8 +7,13 @@ import com.sphirye.springtemplate.service.FencerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -24,5 +29,13 @@ class FencerController {
         @RequestParam(required = false) search: String?,
     ): Page<Fencer> {
         return _fencerService.searchByName(search, pageRequest)
+    }
+
+    @PostMapping("/fencer")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun postFencer(
+        @Validated @RequestBody fencer: Fencer,
+    ): Fencer {
+        return _fencerService.create(fencer)
     }
 }
