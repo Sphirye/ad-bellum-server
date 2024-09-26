d2871e46f67bfdb3898e2d7fb01fd477f2381bf5
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-26 00:46:08 -0500

    Add Auditing.kt

---
+++ src/main/kotlin/com/sphirye/springtemplate/model/Auditing.kt
@@ -0,0 +1,27 @@
+package com.sphirye.springtemplate.model
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+open class Auditing : Serializable {
+
+    @CreatedDate
+    var createdDate: LocalDateTime? = null
+
+    @LastModifiedDate
+    var lastModifiedDate: LocalDateTime? = null
+
+    @CreatedBy
+    var createdBy: Long? = null
+
+    @LastModifiedBy
+    var lastModifiedBy: Long? = null
+}
