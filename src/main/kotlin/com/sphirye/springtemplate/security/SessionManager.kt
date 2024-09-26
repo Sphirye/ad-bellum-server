91a945af6dfe10d7bc445e723c30f5656bc922e7
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-26 00:41:50 -0500

    Refactor SessionManager.kt

--- src/main/kotlin/com/sphirye/springtemplate/security/SessionManager.kt
+++ src/main/kotlin/com/sphirye/springtemplate/security/SessionManager.kt
@@ -1,17 +1,31 @@
 package com.sphirye.springtemplate.security
 
+import com.sphirye.springtemplate.model.UserIdentity
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
 class SessionManager {
 
-    fun getUserEmail(): String {
-        return SecurityContextHolder.getContext().authentication.principal.toString()
+    fun getUserIdentity(): UserIdentity {
+        return SecurityContextHolder.getContext().authentication.principal as UserIdentity
     }
 
-    fun getUserAuthorities(): Any? {
-        return SecurityContextHolder.getContext().authentication.authorities
+    fun isAuthenticated(): Boolean {
+        return if (_getAuthentication() == null) {
+            false
+        } else {
+            _getAuthentication()?.principal is UserIdentity
+        }
+    }
+
+    fun requestingUserMatchesWithSessionId(id: Long): Boolean {
+        return id == getUserIdentity().id
+    }
+
+    private fun _getAuthentication(): Authentication? {
+        return SecurityContextHolder.getContext().authentication
     }
 
 }
