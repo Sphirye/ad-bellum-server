428f01ef97fc1dc8137a4af33fd844baed29c4cf
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-16 09:50:58 -0500

    Add getRenewedSession method in AuthService.kt

--- src/main/kotlin/com/sphirye/springtemplate/service/AuthService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/service/AuthService.kt
@@ -3,6 +3,7 @@ package com.sphirye.springtemplate.service
 import com.sphirye.springtemplate.model.Session
 import com.sphirye.springtemplate.model.UserCredentials
 import com.sphirye.springtemplate.model.UserIdentity
+import com.sphirye.springtemplate.security.SessionManager
 import com.sphirye.springtemplate.security.util.JwtTokenUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.AuthenticationManager
@@ -26,6 +27,9 @@ class AuthService {
     @Autowired
     private lateinit var _jwtTokenUtil: JwtTokenUtil
 
+    @Autowired
+    private lateinit var _sessionManager: SessionManager
+
     fun login(credentials: UserCredentials): Session {
         val user = _userService.findByEmail(credentials.email)
 
@@ -38,6 +42,16 @@ class AuthService {
         val auth = _authenticationManager.authenticate(
             UsernamePasswordAuthenticationToken(userIdentity, user.password, listOf())
         )
+
+        return Session(
+            token = "Bearer ${_jwtTokenUtil.generateAccessToken(userIdentity.id)}",
+            user = user
+        )
+    }
+
+    fun getRenewedSession(): Session {
+        val userIdentity = _sessionManager.getUserIdentity()
+        val user = _userService.findById(userIdentity.id)
 
         return Session(
             token = "Bearer ${_jwtTokenUtil.generateAccessToken(userIdentity.id)}",
