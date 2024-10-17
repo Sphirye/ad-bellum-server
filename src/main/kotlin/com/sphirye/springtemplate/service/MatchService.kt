c51cae978541b6073f897b0dbad84a7bf1463473
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-16 20:45:58 -0500

    Check if the match attempting to update is already finished

--- src/main/kotlin/com/sphirye/springtemplate/service/MatchService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/service/MatchService.kt
@@ -1,5 +1,7 @@
 package com.sphirye.springtemplate.service
 
+import com.sphirye.shared.exception.exceptions.ApplicationException
+import com.sphirye.shared.exception.exceptions.ConflictException
 import com.sphirye.shared.utils.BaseService
 import com.sphirye.springtemplate.model.Match
 import com.sphirye.springtemplate.repository.MatchRepository
@@ -9,5 +11,12 @@ import org.springframework.stereotype.Service
 class MatchService(
     private val _matchRepository: MatchRepository,
 ): BaseService<Match, Long>(_matchRepository) {
+
+    override fun beforeUpdate(id: Long, entity: Match): Match {
+        if (_matchRepository.existsByIdAndState(id, Match.MatchState.FINISHED)) {
+            throw ConflictException("Finished matches cannot be updated.")
+        }
+        return super.beforeUpdate(id, entity)
+    }
 
 }
