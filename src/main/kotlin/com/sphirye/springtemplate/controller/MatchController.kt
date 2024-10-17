d7acc7af283a83a2ea3da19f8d1c1a2dd7dbf330
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-16 20:46:14 -0500

    Add put endpoint in MatchController.kt

--- src/main/kotlin/com/sphirye/springtemplate/controller/MatchController.kt
+++ src/main/kotlin/com/sphirye/springtemplate/controller/MatchController.kt
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -43,4 +44,12 @@ class MatchController {
         return _matchService.create(match)
     }
 
+    @PutMapping("/match/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun update(
+        @PathVariable id: Long,
+        @Validated @RequestBody match: Match,
+    ): Match {
+        return _matchService.update(id, match)
+    }
 }
