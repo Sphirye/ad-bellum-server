9e5aeed22d02fdaaaa1fdd3dfc34c33169d5a578
Roniel Contreras <ronielcontreras70@gmail.com>
2024-11-07 01:07:07 -0500

    Use JpaSpecificationExecutor in MatchRepository.kt

--- src/main/kotlin/com/sphirye/springtemplate/repository/MatchRepository.kt
+++ src/main/kotlin/com/sphirye/springtemplate/repository/MatchRepository.kt
@@ -2,10 +2,11 @@ package com.sphirye.springtemplate.repository
 
 import com.sphirye.springtemplate.model.Match
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MatchRepository : JpaRepository<Match, Long> {
+interface MatchRepository : JpaRepository<Match, Long>, JpaSpecificationExecutor<Match> {
 
     fun existsByIdAndState(id: Long, state: Match.MatchState): Boolean
 
