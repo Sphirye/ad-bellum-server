39baef3858d0c2f84406a8eb0623d4c0aaaaa09f
Roniel Contreras <ronielcontreras70@gmail.com>
2025-01-02 19:40:15 -0500

    Refactor ScoreProfileService.kt instance method

--- src/main/kotlin/com/sphirye/springtemplate/service/ScoreProfileService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/service/ScoreProfileService.kt
@@ -2,25 +2,18 @@ package com.sphirye.springtemplate.service
 
 import com.sphirye.shared.utils.BaseService
 import com.sphirye.springtemplate.model.ScoreProfile
+import com.sphirye.springtemplate.model.ScoreProfile.ScoreProfileType
 import com.sphirye.springtemplate.repository.ScoreProfileRepository
-import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Service
 
 @Service
 class ScoreProfileService(
     private val _ScoreProfileRepository: ScoreProfileRepository,
-    private val entityManager: EntityManager
 ): BaseService<ScoreProfile, Long>(_ScoreProfileRepository) {
 
-    fun instanceScoreProfile(id: Long): ScoreProfile {
-        val template = findById(id)
-
-        template.id = null
-        template.createdDate = null
-        template.createdBy = null
-
-        entityManager.detach(template)
-        return create(template)
+    fun instance(scoreProfile: ScoreProfile): ScoreProfile {
+        scoreProfile.type = ScoreProfileType.INSTANCE
+        return create(scoreProfile)
     }
 
 }
