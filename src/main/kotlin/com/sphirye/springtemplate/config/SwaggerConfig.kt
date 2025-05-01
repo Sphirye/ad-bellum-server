5bbe345e55740077b8fc4bf7e9900641eeade535
Roniel Contreras <ronielcontreras70@gmail.com>
2025-05-01 11:56:16 -0500

    Add SwaggerConfig.kt

---
+++ src/main/kotlin/com/sphirye/springtemplate/config/SwaggerConfig.kt
@@ -0,0 +1,23 @@
+package com.sphirye.springtemplate.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun customOpenApi(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Ad Bellum API")
+                    .version("1.0")
+                    .description("Documentación de la API")
+            )
+    }
+}
