d08e07df41d031d5b20bea620732c5498693f2b9
Roniel Contreras <ronielcontreras70@gmail.com>
2025-06-28 16:17:59 -0500

    Refactor ScoreProfileController.kt to create penalties exclusively after creation

--- src/main/kotlin/com/sphirye/springtemplate/service/ScoreProfileService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/service/ScoreProfileService.kt
@@ -1,15 +1,28 @@
 package com.sphirye.springtemplate.service
 
 import com.sphirye.shared.utils.BaseService
+import com.sphirye.springtemplate.model.Penalty
+import com.sphirye.springtemplate.model.Penalty.PenaltyType
 import com.sphirye.springtemplate.model.ScoreProfile
 import com.sphirye.springtemplate.model.ScoreProfile.ScoreProfileType
 import com.sphirye.springtemplate.repository.ScoreProfileRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class ScoreProfileService(
     private val _scoreProfileRepository: ScoreProfileRepository,
+    private val _penaltyService: PenaltyService,
 ): BaseService<ScoreProfile, Long>(_scoreProfileRepository) {
+
+    @Transactional
+    override fun afterCreated(entity: ScoreProfile) {
+        entity.penalties?.forEach {
+            it.type = PenaltyType.TEMPLATE
+            it.scoreProfileId = entity.id
+            _penaltyService.create(it)
+        }
+    }
 
     fun instance(scoreProfile: ScoreProfile): ScoreProfile {
         scoreProfile.type = ScoreProfileType.INSTANCE
