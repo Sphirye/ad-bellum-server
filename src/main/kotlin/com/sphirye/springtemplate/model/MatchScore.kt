70a91427cce4e5063af3ccf9a796dd1d7bd1188b
Roniel Contreras <ronielcontreras70@gmail.com>
2025-07-24 17:27:42 -0500

    Refactor penalties field name to penaltyRecords

--- src/main/kotlin/com/sphirye/springtemplate/model/MatchScore.kt
+++ src/main/kotlin/com/sphirye/springtemplate/model/MatchScore.kt
@@ -66,6 +66,6 @@ class MatchScore (
     @JoinColumn(name = "scorer_id", insertable = false, updatable = false)
     var scorer: Fencer? = null
 
-    @OneToMany(mappedBy = "score")
-    var penalties: MutableList<PenaltyRecords>? = null
+    @OneToMany(mappedBy = "score", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var penaltyRecords: MutableList<PenaltyRecords>? = null
 }
